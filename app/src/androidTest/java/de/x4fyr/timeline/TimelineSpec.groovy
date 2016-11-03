1cc259f28b19eaef0ea8bab0e30d07fe3b7f09b9
Benedikt V <benedikt_v@web.de>
2016-11-03 15:44:29 +0100

    Add dummy androidTest

---
+++ app/src/androidTest/java/de/x4fyr/timeline/TimelineSpec.groovy
@@ -0,0 +1,14 @@
+package de.x4fyr.timeline
+
+import spock.lang.Specification
+
+/**
+ * @author Benedikt Volkmer
+ * Created on 11/3/16.
+ */
+class TimelineSpec extends Specification {
+
+    def "dummy test"() {
+        assert true
+    }
+}
