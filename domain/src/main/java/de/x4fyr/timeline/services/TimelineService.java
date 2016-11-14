34205c24c57f5725ac27fa337d3ed473c5fc72f0
Benedikt V <benedikt_v@web.de>
2016-11-14 12:13:44 +0100

    backend: remove dafault methods from TimelineService interface

--- domain/src/main/java/de/x4fyr/timeline/services/TimelineService.java
+++ domain/src/main/java/de/x4fyr/timeline/services/TimelineService.java
@@ -17,37 +17,20 @@ import java.time.LocalDateTime;
 public interface TimelineService<E extends ScheduledElement> {
 
     /**
-     * What is considered as the last hour of the day for default interface methods.
-     */
-    int LAST_HOUR = 23;
-    /**
-     * What is considered as the last minute of the day for default interface methods.
-     */
-    int LAST_MINUTE = 59;
-
-    /**
      * Get the timeline of today.
-     *
-     * <p>Defaults to a call of getDate(LocalDate.now())
      *
      * @return timeline with only elements of today
      */
-    default Timeline<E> getToday() {
-        return this.getDate(LocalDate.now());
-    }
+    Timeline<E> getToday();
 
     /**
      * Get the timeline of a specific date.
-     *
-     * <p>defaults to getTimeSpan of startOfDay to 23:59 of the current day
      *
      * @param date date of retrieval
      *
      * @return timeline with only elements of date
      */
-    default Timeline<E> getDate(LocalDate date) {
-        return getTimeSpan(date.atStartOfDay(), date.atTime(LAST_HOUR, LAST_MINUTE));
-    }
+    Timeline<E> getDate(LocalDate date);
 
     /**
      * Get the timeline of the given time span.
